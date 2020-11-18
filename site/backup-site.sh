d434f75e529afc140294d1d0a2ab832fadfae217
Serhii Kalchenko <sergii.kalchenko@gmail.com>
2020-11-18 21:35:15 +0200

    update...

--- site/backup-site.sh
+++ site/backup-site.sh
@@ -2,6 +2,7 @@
 
 # Backup local running Joomla site
 # Run script as root!
+# Change 'projectDir' to your dir! Change path for 'tar' command! 
 projectDir=~/DevOps/joomla.site
 cd /var/lib/docker/volumes/joomla/_data
 sudo tar -cpjf $projectDir/site/joomla-data.tar.bz2 .
